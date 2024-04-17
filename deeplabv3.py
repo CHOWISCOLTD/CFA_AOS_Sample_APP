dc799c94616548a136c0bd8a6542c4a1cece2a1a
shulichowis <97859083+SLchowis@users.noreply.github.com>
2024-04-17 16:09:11 +0900

    Delete deeplabv3.py

--- deeplabv3.py
+++
@@ -1,8 +0,0 @@
-import torch
-
-model = torch.hub.load('pytorch/vision:v0.7.0', 'deeplabv3_resnet50', pretrained=True)
-model.eval()
-
-scriptedm = torch.jit.script(model)
-torch.jit.save(scriptedm, "deeplabv3_scripted.pt")
-
